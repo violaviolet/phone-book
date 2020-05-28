1a57ba1ec5a63f0677f8d99a11a55be6811a2768
Wiola <wioleta.swist@gmail.com>
2020-05-28 19:35:56 +0200

    New functions and main loop added

---
+++ User.py
@@ -0,0 +1,11 @@
+class User:
+
+    def __init__(self, name, phone_number, creation_date):
+        """Initialization of attributes describing User profile """
+        self.name = name
+        self.phone_number = phone_number
+        self.creation_date = creation_date
+
+    def to_string(self):
+        record = self.name + ' ' + str(self.phone_number) + str(self.creation_date)
+        return record
--- main_functions.py
+++ main_functions.py
@@ -1,15 +1,57 @@
-from class_User import User
+from User import User
 from datetime import date
+import sys
 
 
-def prepare_data():
+def add_new_user(users):
+    #argument inout
     name = input("Enter user name: ")
     phone_number = input("Enter user phone number: ")
     creation_date = date.today()
     new_user = User(name, phone_number, creation_date)
-    print(new_user.get_record())
+    print(new_user.to_string())
+    users.append(new_user)
+
+def show_all_users(users):
+    for user in users:
+        print(user.to_string())
+
+
+def show_user():
+    return
+
+
+#def add_user_group():
+#    return
+
+
+def show_menu():
+    print("----------------------MENU-----------------------------")
+    print("""Choose what you'd like to do :
+                        1. Add new user.
+                        2. Display all users.
+                        3. Display one user.
+                        4. Close program. \n""")
 
 
 
-prepare_data()
+def main_program_loop():
+    users = []
+    while True:
+        show_menu()
+        option = int(input("Enter your choice:"))
+        if option == 1:
+            add_new_user(users)
+        elif option == 2:
+            show_all_users(users)
+        elif option == 3:
+            show_user()
+        elif option == 4:
+            break
+        else:
+            print("Option was not recognized. Try again!")
+
+
+
+main_program_loop()
 
