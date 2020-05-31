4ac43f1e81b7ee40d38a8cc025472609579c8373
Wiola <wioleta.swist@gmail.com>
2020-05-31 19:23:25 +0200

    search by user name and surname functions added

--- main_functions.py
+++ main_functions.py
@@ -2,7 +2,6 @@ from User import User
 from datetime import datetime
 
 
-
 def load_data_from_file(users, filepath):
     file = open(filepath, "r")
     for line in file:
@@ -10,7 +9,8 @@ def load_data_from_file(users, filepath):
         name = user_as_list_of_strings[0]
         surname = user_as_list_of_strings[1]
         phone_number = int(user_as_list_of_strings[2])
-        date_time = datetime.strptime(user_as_list_of_strings[3] + " " + user_as_list_of_strings[4].rstrip(), '%Y-%m-%d %H:%M:%S')
+        date_time = datetime.strptime(user_as_list_of_strings[3] + " " + user_as_list_of_strings[4].rstrip(),
+                                      '%Y-%m-%d %H:%M:%S')
         new_user = User(name, surname, phone_number, date_time)
         users.append(new_user)
 
@@ -37,10 +37,33 @@ def show_all_users(users):
         print(user.to_string())
 
 
-def show_user(users):
+"""def show_user(users):
     searching_filter = input("Enter searching filter:")
     for user in users:
         print(user)
+"""
+
+
+def search_user_by_name(users):
+    name = input("Enter user name:")
+    at_least_one_result_found = False
+    for user in users:
+        if user.name == name:
+            print(user.to_string())
+            at_least_one_result_found = True
+    if not at_least_one_result_found:
+        print("User does not exist")
+
+
+def search_user_by_surname(users):
+    surname = input("Enter user surname:")
+    at_least_one_result_found = False
+    for user in users:
+        if user.surname == surname:
+            print(user.to_string())
+            at_least_one_result_found = True
+    if not at_least_one_result_found:
+        print("User does not exist")
 
 
 def show_menu():
@@ -50,6 +73,13 @@ def show_menu():
                         2. Display all users.
                         3. Display one user.
                         4. Close program. \n""")
+
+
+def show_filtering_options_menu():
+    print("-----------------Looking--For--A-User------------")
+    print("""Choose what you'd like to do:
+                  1. Search a user by a name.
+                  2. Search a user by a surname. """)
 
 
 def main_program_loop():
@@ -64,7 +94,14 @@ def main_program_loop():
         elif option == 2:
             show_all_users(users)
         elif option == 3:
-            show_user(filepath)
+            show_filtering_options_menu()
+            option1 = int(input("Enter your choice:"))
+            if option1 == 1:
+                search_user_by_name(users)
+            elif option1 == 2:
+                search_user_by_surname(users)
+            else:
+                return
         elif option == 4:
             break
         else:
@@ -72,4 +109,3 @@ def main_program_loop():
 
 
 main_program_loop()
-
