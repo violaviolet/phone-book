eb04a9719483bb84ab706ab270a828f7c6e9c8ec
Wiola <wioleta.swist@gmail.com>
2020-06-08 20:39:06 +0200

    Rafctor due to addition FileStorage class: removing functions, refactoring main_program_loop

--- phonebook/main_functions.py
+++ phonebook/main_functions.py
@@ -1,5 +1,6 @@
 from User import User
 from phonebook.UI.TerminalUserInterface import TerminalUserInterface
+from phonebook.PersistentDataStorage.FileStorage import FileStorage
 from datetime import datetime
 from enum import Enum
 
@@ -10,31 +11,11 @@ class Search(Enum):
     PHONE_NUMBER = 3
 
 
-def load_data_from_file(users, filepath):
-    file = open(filepath, "r")
-    for line in file:
-        user_as_list_of_strings = line.split(" ")
-        name = user_as_list_of_strings[0]
-        surname = user_as_list_of_strings[1]
-        phone_number = int(user_as_list_of_strings[2])
-        date_time = datetime.strptime(user_as_list_of_strings[3] + " " + user_as_list_of_strings[4].rstrip(),
-                                      '%Y-%m-%d %H:%M:%S')
-        new_user = User(name, surname, phone_number, date_time)
-        users.append(new_user)
-
-
 def add_new_user(users, user_as_dictionary):
     creation_date = datetime.now()
     new_user = User(user_as_dictionary["name"], user_as_dictionary["surname"], user_as_dictionary["phone_number"], creation_date)
     users.append(new_user)
     return new_user
-
-
-def add_to_file(filepath, user):
-    """Add new user to file.txt"""
-    file = open(filepath, "a")
-    file.writelines(user.to_string() + "\n")
-    file.close()
 
 
 def search_user(user_data, users, field):
@@ -52,16 +33,16 @@ def search_user(user_data, users, field):
 
 
 def main_program_loop():
-    users = []
-    filepath = r"..\data\base.txt"
-    load_data_from_file(users, filepath)
     tui = TerminalUserInterface()
+    filepath = r"..\data\base.txt"
+    file_storage = FileStorage(filepath)
+    users = file_storage.load()
     while True:
         option = tui.get_action()
         if option == 1:
             user_data = tui.get_new_user_data()
             user = add_new_user(users, user_data)
-            add_to_file(filepath, user)
+            file_storage.save(filepath, user)
         elif option == 2:
             tui.show_users(users)
         elif option == 3:
